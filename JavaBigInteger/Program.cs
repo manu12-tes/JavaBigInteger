c8b0ad914169f80f42e45636eea3a36d8c71f233
Manuel Garcia <kiramanuel123@gmail.com>
2021-05-14 23:36:02 -0500

    Agregar archivos de proyecto.

---
+++ JavaBigInteger/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JavaBigInteger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dame dos numeros");
+            int a = Int32.Parse(Console.ReadLine());
+            int b = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine((a + b) + "\n"+(a*b));
+        }
+    }
+}
